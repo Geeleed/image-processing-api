372dce8bd440eb0f51557bdbc1a5ad8d7d6e590c
Geeleed <surasak.kaewpho@gmail.com>
2023-10-10 07:37:54 +0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import cv2
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -19,3 +19,55 @@ app.add_middleware(
 @app.get('/')
 async def root():
     return '<Geeleed/> สวัสดีครับ api นี้ใช้ประมวลผลภาพ สามารถดูรายละเอียดได้ที่ /docs'
+
+# รับภาพมาคำนวณสัดส่วนแสงสี
+@app.post('/photo-value/')
+async def photoValue(file:UploadFile):
+    image_data = await file.read()
+    img = Image.open(BytesIO(image_data))
+    img_array = np.array(img)
+    # คำนวณปริมาณของสีแต่ละสีในภาพ
+    red_mean = np.mean(img_array[:, :, 0])  # สีแดง
+    green_mean = np.mean(img_array[:, :, 1])  # เขียว
+    blue_mean = np.mean(img_array[:, :, 2])  # น้ำเงิน
+    # คำนวณความสว่างของภาพในช่วง 0-1
+    brightness_normalized = np.mean(img_array) / 255.0
+    return {
+        "red_percentage": (red_mean / 255) * 100,
+        "green_percentage": (green_mean / 255) * 100,
+        "blue_percentage": (blue_mean / 255) * 100,
+        "brightness_normalized": brightness_normalized
+    }
+
+def compute_histogram(image_np):
+    if image_np.shape[-1] == 3:
+        image_gray = np.dot(image_np[...,:3], [0.2989, 0.5870, 0.1140])
+    else:
+        image_gray = image_np
+    hist = np.histogram(image_gray, bins=256, range=(0, 256))[0]
+    mean = np.mean(hist)
+    sd = np.std(hist)
+    skewness = ((hist - mean) ** 3).sum() / (len(hist) * sd ** 3)
+    kurtosis = ((hist - mean) ** 4).sum() / (len(hist) * sd ** 4)
+    return {
+        "mean": mean,
+        "sd": sd,
+        "skewness": skewness,
+        "kurtosis": kurtosis,
+        "histogram": hist.tolist()
+    }
+
+# วิเคราะห์ histogram ของภาพ
+@app.post('/photo-hist-data/')
+async def photoHistData(file: UploadFile):
+    image_bytes = await file.read()
+    image_np = np.array(Image.open(BytesIO(image_bytes)))
+    
+    result = {
+        "grayscale": compute_histogram(image_np),
+        "red": compute_histogram(image_np[..., 0]),
+        "green": compute_histogram(image_np[..., 1]),
+        "blue": compute_histogram(image_np[..., 2])
+    }
+
+    return result
